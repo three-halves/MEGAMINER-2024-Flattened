fed798b7b0fcaf8e92669d9fde468b822d28f02e
QuasiQ <72662820+QuasiQ@users.noreply.github.com>
2024-11-16 16:51:30 -0600

    Update isPathable

--- Joueur.py/games/magomachy/tile.py
+++ Joueur.py/games/magomachy/tile.py
@@ -113,7 +113,7 @@ class Tile(GameObject):
             bool: True if pathable, False otherwise.
         """
         # <<-- Creer-Merge: is_pathable_builtin -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
-        return False # DEVELOPER ADD LOGIC HERE
+        return not(self.type == "wall" or self.wizard or self.object and self.object.form == "stone") # DEVELOPER ADD LOGIC HERE
         # <<-- /Creer-Merge: is_pathable_builtin -->>
 
     def has_neighbor(self, tile: 'games.magomachy.tile.Tile') -> bool:
